5d5b64bc0532443d7323a47c0204f44f2eef0d63
Eliúde Vemba <sweeteliude@gmail.com>
2023-03-09 23:45:43 +0100

    Add settings to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react'
 import * as A from './style'
 
 const App = () => {
-	const [peso, setPeso] = useState('');
-	const [altura, setAltura] = useState('');
+	const [peso, setPeso] = useState(0);
+	const [altura, setAltura] = useState(0);
 	const [msg, setMsg] = useState('');
 
 	function calc() {
-		if(isNaN(Number(altura)) || isNaN(Number(peso))) return
+		if (isNaN(Number(altura)) || isNaN(Number(peso))) return
 
 		const alt = Number(altura) / 100;
 		const imc = Number(peso) / (alt * alt);
@@ -26,36 +26,55 @@ const App = () => {
 
 	return (
 		<A.AppScreen>
-			<h1>Calculadora IMC</h1>
-			<span>Vamos calcular o seu imc</span>
+			<h1 className='sm:text-5xl text-2xl text-center font-bold'>Calculadora IMC</h1>
+			<span className='text-center'>Vamos calcular o seu imc</span>
 
 			<A.AppInputArea>
-				<input
-					type='range'
-					min={10}
-					max={500}
-					placeholder='Peso em (kg)'
-					value={peso}
-					onChange={(e) => {
-						if (isNaN(Number(e.target.value))) setPeso('')
-						else setPeso(e.target.value)
-					}}
-				/>
+				<div className='flex sm:flex-row flex-col items-center mt-5 gap-5'>
+					<div className='flex items-center'>
+						<span>Peso</span>
+						<span>: {peso} kg</span>
+					</div>
+					<input
+						type='range'
+						min={0}
+						max={500}
+						placeholder='Peso em (kg)'
+						value={peso}
+						onChange={(e) => {
+							if (msg) setMsg('')
+							setPeso(Number(e.target.value))
+						}}
+					/>
+				</div>
 
-				<input
-					type='range'
-					min={50}
-					max={300}
-					placeholder='Altura em (cm)'
-					value={altura}
-					onChange={(e) => {
-						if (isNaN(Number(e.target.value))) setAltura('')
-						else setAltura(e.target.value)
-					}}
-				/>
-				<output>24</output>
+				<div className='flex sm:flex-row flex-col items-center mt-5 gap-5'>
+					<div className='flex items-center'>
+						<span>Altura</span>
+						<span>: {altura} cm</span>
+					</div>
+					<input
+						type='range'
+						min={0}
+						max={300}
+						placeholder='Altura em (cm)'
+						value={altura}
+						onChange={(e) => {
+							if (msg) setMsg('')
+							setAltura(Number(e.target.value))
+						}}
+					/>
+				</div>
 
-				<button onClick={calc}>Calcular</button>
+				<button
+					className={`
+						hover:scale-[1.01] duration-300 origin-bottom
+						${(altura > 0 && peso > 0) ? 'scale-100' : 'scale-0'}
+					`}
+					onClick={calc}
+				>
+					Calcular
+				</button>
 			</A.AppInputArea>
 
 			<h2>{msg}</h2>
--- src/style.ts
+++ src/style.ts
@@ -8,10 +8,6 @@ export const AppScreen = styled.div`
     align-items: center;
     flex-direction: column;
 
-    h1 {
-        font-size: 50px;
-    }
-
     h2 {
         margin-top: 28px;
     }
@@ -22,30 +18,21 @@ export const AppScreen = styled.div`
 `
 
 export const AppInputArea = styled.div`
-    width: 500px;
+    width: 100%;
+    padding: 5px;
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    input {
-        width: 100%;
-        margin-bottom: 12px;
-        height: 46px;
-        background-color: #121212;
-        border: 0;
-        border-radius: 6px;
-        color: #FFF;
-        padding: 0 12px;
-        text-align: center;
-        font-size: 18px;
-    }
-
     button {
         width: 100%;
+        max-width: 250px;
+        padding: 8px 10px;
+        margin: 50px 0 0 0;
         height: 46px;
         border: 0;
-        border-radius: 6px;
-        background-color: #357AFF;
+        border-radius: 16px;
+        background-color: black;
         color: #FFF;
         font-size: 18px;
     }
